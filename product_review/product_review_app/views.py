ba6bb970b43b1c3987ba609791c702de9e252765
Harsha N Hegde <harsha.hegde@agiliti.in>
2021-09-19 23:31:36 +0530

    styled as per figma image

--- product_review/product_review_app/views.py
+++ product_review/product_review_app/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 from .models import Product, ProductReview
 from django.template import loader
 from django.urls import reverse
+from statistics import mean
 
 
 def index(request):
     product_review_list = ProductReview.objects.all()
+    rating_list = list()
+    for pr in product_review_list:
+        rating_list.append(pr.rating)
+
+    average_rating = round(mean(rating_list), 1)
+    product_title = Product.objects.get(pk=1).title
+
     template = loader.get_template('review.html')
     context = {
+        'product_title': product_title,
+        'average_rating': average_rating,
         'product_review_list': product_review_list
     }
     return HttpResponse(template.render(context, request))
