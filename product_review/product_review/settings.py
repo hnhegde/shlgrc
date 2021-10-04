bb270848e4cf9fc8b2121a88ed808ec7d1521080
Harsha N Hegde <hnhegde@users.noreply.github.com>
2021-10-04 15:56:30 +0530

    removed security key
    this will be put in .env file

--- product_review/product_review/settings.py
+++ product_review/product_review/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&mk60jw^7w9thr2jhgj-ffb78br_6j+ku38w^rm6e+9jjn-n*8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
